ba34abb7ba2982e9f549de12379a8409e4849f64
Vaishnavi ravichandran <ravichandran.vaishu@gmail.com>
2020-10-22 15:39:09 -0500

    table

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import Table from './table'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <h1>Hello, React!</h1>
+        <Table/>
       </div>
     )
   }
---
+++ src/table.js
@@ -0,0 +1,36 @@
+import React, {Component} from 'react'
+//import ReactDOM from 'react-dom'
+import './index.css'
+
+class Table extends React.Component{
+// eslint-disable-next-line no-useless-constructor
+constructor(props){
+    super(props)
+}
+
+render(){
+return (
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Job</th>
+                </tr>
+            </thead>
+        </table>
+    </div>
+)
+
+
+
+}
+
+
+
+
+}
+
+export default Table 
+
+
