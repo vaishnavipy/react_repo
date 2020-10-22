5c21d522a2943042bb993c4db8b540783143785f
Vaishnavi ravichandran <ravichandran.vaishu@gmail.com>
2020-10-22 15:54:08 -0500

    using_map

--- src/index.js
+++ src/index.js
@@ -5,10 +5,28 @@ import Table from './table'
 
 class App extends Component {
   render() {
+    const characters = [
+      {
+        name: 'Charlie',
+        job: 'Janitor',
+      },
+      {
+        name: 'Mac',
+        job: 'Bouncer',
+      },
+      {
+        name: 'Dee',
+        job: 'Aspring actress',
+      },
+      {
+        name: 'Dennis',
+        job: 'Bartender',
+      },
+    ]
     return (
       <div className="container">
         <h1>Hello, React!</h1>
-        <Table/>
+        <Table characterData={characters}/>
       </div>
     )
   }
--- src/table.js
+++ src/table.js
@@ -2,6 +2,40 @@ import React, {Component} from 'react'
 //import ReactDOM from 'react-dom'
 import './index.css'
 
+function TableHead(){
+    
+return (
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Job</th>
+                </tr>
+            </thead>
+
+)
+
+
+}
+
+function TableBody(props){
+const characters = props.characterData.map(function(e,i){
+return (<tr>
+        <td>{e.name}</td>
+        <td>{e.job}</td>
+        </tr>)
+
+
+})
+
+return (
+        <tbody>{characters}</tbody>
+
+)
+
+}
+
+
+
 class Table extends React.Component{
 // eslint-disable-next-line no-useless-constructor
 constructor(props){
@@ -9,15 +43,12 @@ constructor(props){
 }
 
 render(){
+    const {characterData} = this.props;
 return (
     <div>
-        <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Job</th>
-                </tr>
-            </thead>
+        <table id="customers">
+            <TableHead />
+            <TableBody characterData={characterData}/>
         </table>
     </div>
 )
