7c17cff6ad27d68364787ce8b334bb41da2a2068
Vaishnavi ravichandran <ravichandran.vaishu@gmail.com>
2020-10-22 20:08:24 -0500

    wikipedia_api

--- src/index.js
+++ src/index.js
@@ -1,43 +1,50 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import Table from './table'
-import Form from './form'
 
+//This App pulls data for the wikipeadi API and renders it as an unordered List   
 class App extends Component {
   state={
-    characters: []
-    
-  }
-  removeCharacter =(index) =>{
-    const {characters} = this.state;
-
-    this.setState({characters : characters.filter(function(e,i){
-      return i !== index
-    })})
-
-
-  }
-
-  handleSubmit=(character)=>{
-  
-    this.setState({characters: [...this.state.characters, character]})
-
-
+    result:[]
   }
 
 
 
-  render() {
-   
-    return (
-      <div className="container">
-        <h1>Hello, React!</h1>
-        <Table characterData={this.state.characters} removeCharacter={this.removeCharacter}/>
-        <Form handleSubmit={this.handleSubmit}/>
-      </div>
-    )
+  componentDidMount() {
+
+    const  url= 'https://en.wikipedia.org/w/api.php?action=opensearch&search=Seona+Dancing&format=json&origin=*';
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data=> {
+      console.log(data)
+      this.setState({result:data})
+
+    })
+
+
+
+
   }
+
+
+
+
+render(){
+  const {result} = this.state;
+  //console.log(result)
+  const my_list = result.map(function(e,i){
+  return <li key={i}>{e}</li>
+  })
+
+  return(
+    <div>
+      <ul>{my_list}</ul>
+    </div>
+  )
+
+}
+ 
 }
 
 ReactDOM.render(<App />, document.getElementById('content'))
